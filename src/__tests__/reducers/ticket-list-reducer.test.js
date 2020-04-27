399effb6ff7e30a99204289f5365a3294c8355f5
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-27 13:39:27 -0700

    fail third test
    Co-authored by: Steph Podolak <StephPodolak@gmail.com>

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -3,11 +3,17 @@ import ticketListReducer from '../../reducers/ticket-list-reducer';
 describe('ticketListReducer', () => {
 
   let action;
-  const ticketData = {
-    names: 'Ryan & Aimen',
-    location: '4b',
-    issue: 'Redux action is not working correctly.',
-    id: 1
+  const currentState = {
+    1:{
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly.',
+      id: 1},
+    2:{
+      names: 'Jasmine and Justine',
+      location: '2a',
+      issue: 'Reducer has side effects.',
+      id: 2},
   };
 
   test('Should return default state if there is no action type passed into the reducer', () => {
@@ -15,7 +21,7 @@ describe('ticketListReducer', () => {
   });
 
   test('Should successfully add new ticket data to masterTicketList', () => {
-    const {names, location, issue, id } = ticketData;
+    const {names, location, issue, id } = currentState;
     action = {
       type: 'ADD_TICKET',
       names: names,
@@ -30,6 +36,19 @@ describe('ticketListReducer', () => {
         issue: issue,
         id: id
       }
+    });
+  });
+
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2 }
     });
   });
 
